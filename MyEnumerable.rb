41247b88c5c97efd2ad92a4b8e9ef42b99f8be57
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-03 02:21:41 +0300

    modules and enumerables added

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,30 +1,21 @@
-# module MyEnumerable
-#   def all?(&block)
-#     each { |item| return false unless block.call(item) }
-#     true
-#   end
-# end
+module MyEnumable
+  def all?
+      list.each do |e|
+        return false unless yield e
+      end
+      true
+  end 
 
-# module MyEnumable
-#   def all?
-#       list.each do |e|
-#         return false unless yield e
-#       end
-#       true
-#   end 
+  def any?
+      list.each do |e|
+        return true if yield e
+      end
+      false
+  end 
 
-#   def any?
-#       list.each do |e|
-#         return true if yield e
-#       end
-#       false
-#   end 
-
-#   def filter
-#     result = []
-#     list.each { |e| result << e if yield e }
-#     return result
-
-#     each 
-#   end 
-# end
+  def filter
+    result = []
+    list.each { |e| result << e if yield e }
+    return result
+  end 
+end
